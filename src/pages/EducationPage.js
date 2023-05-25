51c1c39f478fe3f46b6fa97932bf3b25ecae04d1
Aaryan Baral <aryanbaral1100@gmail.com>
2023-05-25 11:17:40 +0545

    Education Page made

--- src/pages/EducationPage.js
+++ src/pages/EducationPage.js
@@ -1,9 +1,99 @@
-import React from 'react'
+import React from "react";
+import "../styles/Education.css";
 
 export default function EducationPage() {
   return (
-    <div>
-      
+    <div className="education-page">
+      <div className="box" id="education">
+        <div className="edu-title">
+          <h1>Education</h1>
+          <div className="box-container">
+            <div className="contents">
+              <div className="sub-contents">
+                <h4>2019-2023</h4>
+                <h2>Nepal College of Information Technology</h2>
+                <h4>Bachelor of Software Engineering</h4>
+                <p>
+                  Currently pursuing my Bachelors in Computer Science and
+                  Information Technology from University of Calcutta.
+                </p>
+              </div>
+            </div>
+            <div className="contents">
+              <div className="sub-contents">
+                <h4>2019-2023</h4>
+                <h2>National School of Sciences</h2>
+                <h4> High School </h4>
+                <p>
+                  Currently pursuing my Bachelors in Computer Science and
+                  Information Technology from University of Calcutta.
+                </p>
+              </div>
+            </div>
+            <div className="contents">
+              <div className="sub-contents">
+                <h4>2019-2020</h4>
+                <h2>Gorakshya Nikhil Jyoti Divya Vidyashram</h2>
+                <h4>Secondary School </h4>
+                <p>
+                  Currently pursuing my Bachelors in Computer Science and
+                  Information Technology from University of
+                  Calcutta.alsmdclkamsdcadcadsca
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="box" id="experience">
+        <div className="edu-title">
+          <h1>Experience</h1>
+          <div className="box-container">
+            <div className="contents">
+              <div className="sub-contents">
+                <h4>2019-2023</h4>
+                <h2>Startup Company</h2>
+                <h4>Backend Developer Intern</h4>
+                <p>
+                  Currently pursuing my Bachelors in Computer Science and
+                  Information Technology from University of Calcutta.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="box" id="projects">
+        <div className="edu-title">
+          <h1>Projects</h1>
+          <div className="box-container">
+            <div className="contents">
+              <h2>Title</h2>
+              <p>
+                Description
+              </p>
+              <p>
+                Link
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="edu-title">
+          <h1>Projects</h1>
+          <div className="box-container">
+            <div className="contents">
+              <h2>Title</h2>
+              <p>
+                Description
+              </p>
+              <p>
+                Link
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
-  )
+  );
 }
